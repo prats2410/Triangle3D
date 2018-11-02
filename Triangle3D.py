b80ae89a3a0a8f18af85d07f5146372a7dd71f2d
prats2410 <44079665+prats2410@users.noreply.github.com>
2018-11-02 09:30:49 +0530

    Add files via upload

---
+++ Triangle3D.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+speed(0)
+for i in range(50):
+    forward(200)
+    left(90)
+    forward(200)
+    left(135)
+    x=(200**2+200**2)**0.5
+    forward(x)
+    left(135)
+    left(22.5)
+    forward(3)
+    right(22.5)
